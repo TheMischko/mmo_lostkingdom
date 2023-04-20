d155de9eb2730d85de3e9cc04b21ecaba9324d1d
Miško Dvořák <michal.dvorak999@gmail.com>
2023-04-21 00:38:59 +0200

    Client: Added new chat handler.

---
+++ Assets/Scripts/Networking/MessageSenders/NewChatMessagesHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Networking.UI;
+using Shared.DataClasses;
+using Shared.Utils;
+using UnityEngine;
+
+namespace Networking.MessageSenders {
+    public static class NewChatMessagesHandler {
+        public static event EventHandler<ChatMessage[]> Happened;
+        public static void Handle(BufferReader reader) {
+            int count = reader.ReadInt();
+            ChatMessage[] messages = new ChatMessage[count];
+            for (int i = 0; i < count; i++) {
+                ChatMessage message = new ChatMessage();
+                message = (ChatMessage)message.FromBytes(reader);
+                ChatManager.instance.AddMessage(message);
+                messages[i] = message;
+            }
+            Happened?.Invoke(messages);
+        }
+    }
+}
