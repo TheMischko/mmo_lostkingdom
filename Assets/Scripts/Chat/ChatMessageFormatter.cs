5352b8d1eb7159ec35723b662294f1e5721dfa5b
Miško Dvořák <michal.dvorak999@gmail.com>
2023-04-21 12:45:33 +0200

    Client: Added class for formatting chat messages.

---
+++ Assets/Scripts/Chat/ChatMessageFormatter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using Networking;
+using Shared.DataClasses;
+using Shared.Enums;
+using UnityEngine;
+
+namespace Chat {
+    public class ChatMessageFormatter {
+        private UserManager userManager;
+        private delegate string MessageFormatterMethod(ChatMessage message);
+
+        private Dictionary<ChatMessageType, MessageFormatterMethod> messageFormatters;
+
+        private string messageDateFormat = "hh:mm";
+
+        public ChatMessageFormatter() {
+            userManager = UserManager.instance;
+            messageFormatters =  new Dictionary<ChatMessageType, MessageFormatterMethod>();
+            
+            messageFormatters.Add(ChatMessageType.Normal, FormatNormalMessage);
+            messageFormatters.Add(ChatMessageType.Server, FormatServerMessage);
+            messageFormatters.Add(ChatMessageType.Admin, FormatAdminMessage);
+        }
+
+        public string Format([CanBeNull] ChatMessage message) {
+            if (message == null) {
+                return "";
+            }
+            return messageFormatters[message.messageType](message);
+        }
+
+        private static string ColorText(string text, Color color) {
+            return $"<color=#{ColorUtility.ToHtmlStringRGBA(color)}>{text}</color>";
+        }
+
+        private string FormatNormalMessage(ChatMessage message) {
+            return string.Format("{0} {1}: {2}", 
+                ColorText(message.timeReceived.ToString(messageDateFormat), ChatColors.dateColor),
+                ColorText(userManager.GetUser(message.socketIndex).name, ChatColors.userColor),
+                ColorText(message.content, ChatColors.messageColor)
+                );
+        }
+        private string FormatAdminMessage(ChatMessage message) {
+            return ColorText(
+                string.Format("<b>{0} [GM]{1}: {2}</b>",
+                        message.timeReceived.ToString(messageDateFormat),
+                        userManager.GetUser(message.socketIndex).name,
+                        message.content
+                        ),
+                ChatColors.adminColor
+            );
+        }
+
+        private string FormatServerMessage(ChatMessage message) {
+            return ColorText($"{message.timeReceived.ToString(messageDateFormat)} <i>Server: {message.content}</i>", 
+                ChatColors.serverColor);
+        }
+    }
+}
