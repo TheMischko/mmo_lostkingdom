972296711800f5814259021ff6f09722525d8446
Miško Dvořák <michal.dvorak999@gmail.com>
2023-04-21 12:44:16 +0200

    Client: Added chat message colors.

---
+++ Assets/Scripts/Chat/ChatColors.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Chat {
+    public static class ChatColors {
+        public static Color dateColor = GetColorRGBA(168,175,184, 1f);
+        public static Color userColor = GetColorRGBA(102, 0, 102, 1f);
+        public static Color messageColor = GetColorRGBA(238, 238, 238, 1f);
+        public static Color adminColor = GetColorRGBA(107, 228, 223, 1f);
+        public static Color serverColor = GetColorRGBA(250, 128, 114, 1f);
+
+        private static Color GetColorRGBA(int red, int green, int blue, float alpha) {
+            return new Color((float)red / 255, (float)green / 255, (float)blue / 255, alpha);
+        }
+    }
+}
