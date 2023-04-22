79f6f2cfb678ec0011d7d878dcad74b0c9203f01
Miško Dvořák <michal.dvorak999@gmail.com>
2023-04-22 12:08:15 +0200

    Server: LoginHandler now async and notifies others.

--- Server/Server/MessageHandlers/LoginHandler.cs
+++ Server/Server/MessageHandlers/LoginHandler.cs
@@ -1,19 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using Server.MessageSenders;
+using Server.Model.ChatModel;
+using ServiceStack;
+using Shared.DataClasses;
 using Shared.Utils;
 
 namespace Server.MessageHandlers {
     public static class LoginHandler {
-        public static void Handle(int index, BufferReader reader) {
+        public static async void Handle(int index, BufferReader reader) {
             string login = reader.ReadString();
             string password = reader.ReadString();
             Console.WriteLine($"{login} is logging in with password {password}");
             
             // Handle login
             
-            LoginSuccessfulSender.SendMessage(index);
+            await LoginSuccessfulSender.SendMessage(index);
+
+            ChatModel.instance.AddNewChatListener(index);
             
+            List<UserData> users = Network.clients.Map(c => c.userData);
+            foreach (UserData user in users) {
+                if(user == null) continue;
+                if(user.index == index) continue;
+                await PlayerConnectedSender.SendMessage(index, user);
+            }
+            await PlayerConnectedSender.BroadcastMessage(Network.clients[index].userData, new []{index});
         }
     }
 }
