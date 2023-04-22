e29c5f387bbdb805952f22feddb9634436ee91bc
Miško Dvořák <michal.dvorak999@gmail.com>
2023-04-22 13:39:27 +0200

    Server: Created ResultInfo generic class for results with status.

---
+++ Server/Server/Utils/ResultInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace Server.Utils {
+    public struct ResultInfo<T> {
+        public Status status;
+        public T content;
+        public string message;
+        
+        public ResultInfo(Status status, T content, string message = "") {
+            this.status = status;
+            this.content = content;
+            this.message = message;
+        }
+    }
+}
