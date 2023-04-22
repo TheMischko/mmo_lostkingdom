d6fcdcfd90dd44e81822dbabdac309e85e7ff632
Miško Dvořák <michal.dvorak999@gmail.com>
2023-04-22 12:09:27 +0200

    Server: PlayerConnected moved to new Sender class.

--- Server/Server/MessageSenders/PlayerConnectedSender.cs
+++ Server/Server/MessageSenders/PlayerConnectedSender.cs
@@ -1,5 +1,24 @@
-﻿namespace Server.MessageSenders {
+﻿using System.Threading.Tasks;
+using Shared.DataClasses;
+using Shared.Utils;
+
+namespace Server.MessageSenders {
     public class PlayerConnectedSender {
-        
+        private const int instruction = 101;
+
+        public static async Task SendMessage(int index, UserData newUser) {
+            BufferWriter bw = new BufferWriter();
+            bw.WriteInt(instruction);
+            bw.AddData(newUser.ToBytes());
+
+            await Network.instance.SendToClientAsync(index, bw.ToArray());
+        }
+
+        public static async Task BroadcastMessage(UserData newUser, int[] skipIndicies) {
+            BufferWriter bw = new BufferWriter();
+            bw.WriteInt(instruction);
+            bw.AddData(newUser.ToBytes());
+            await Network.instance.Broadcast(bw.ToArray(), skipIndicies);
+        }
     }
 }
