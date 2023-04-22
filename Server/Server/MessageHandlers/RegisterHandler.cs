225982e3731a16a25bd3a81b70f6b8d7d537e078
Miško Dvořák <michal.dvorak999@gmail.com>
2023-04-22 13:46:34 +0200

    Server: Created registration handler that uses UserModel.

--- Server/Server/MessageHandlers/RegisterHandler.cs
+++ Server/Server/MessageHandlers/RegisterHandler.cs
@@ -1,5 +1,21 @@
-﻿namespace Server.MessageHandlers {
+﻿using Server.Model.UserModel;
+using Server.Utils;
+using Shared.DataClasses;
+using Shared.Utils;
+
+
+namespace Server.MessageHandlers {
     public class RegisterHandler {
-        
+        public static async void Handle(int index, BufferReader reader) {
+            string login = reader.ReadString();
+            string email = reader.ReadString();
+            string passwordRaw = reader.ReadString();
+
+            ResultInfo<long> result = await UserModel.instance.Register(login, email, passwordRaw);
+            if (result.status == Status.Error) {
+                // Handle error.
+                return;
+            }
+        }
     }
 }
