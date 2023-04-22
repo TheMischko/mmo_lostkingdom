0dbb5166de2fe3ffa9c5222d33bcccf49aa7ab2b
Miško Dvořák <michal.dvorak999@gmail.com>
2023-04-22 12:08:58 +0200

    Server: LoginSuccessful is now async.

--- Server/Server/MessageSenders/LoginSuccessfulSender.cs
+++ Server/Server/MessageSenders/LoginSuccessfulSender.cs
@@ -1,17 +1,18 @@
-﻿using Shared.DataClasses;
+﻿using System.Threading.Tasks;
+using Shared.DataClasses;
 using Shared.Utils;
 
 namespace Server.MessageSenders {
     public static class LoginSuccessfulSender {
         private const int instruction = 102;
-        public static void SendMessage(int index) {
+        public static async Task SendMessage(int index) {
             BufferWriter bw = new BufferWriter();
             bw.WriteInt(instruction);
             string userName = $"user-{index}";
             UserData userData = new UserData(index, userName);
             bw.AddData(userData.ToBytes());
             
-            Network.instance.SendToClient(index, bw.ToArray());
+            await Network.instance.SendToClientAsync(index, bw.ToArray());
         }
     }
 }
