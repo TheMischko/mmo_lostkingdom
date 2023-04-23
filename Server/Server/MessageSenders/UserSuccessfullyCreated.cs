5dd2a195764569ce14583ab14e99764273f2d8ac
Miško Dvořák <michal.dvorak999@gmail.com>
2023-04-23 12:56:57 +0200

    Server: Added sender for successful registration.

--- Server/Server/MessageSenders/UserSuccessfullyCreated.cs
+++ Server/Server/MessageSenders/UserSuccessfullyCreated.cs
@@ -1,5 +1,17 @@
-﻿namespace Server.MessageSenders {
-    public class UserSuccessfullyCreated {
-        
+﻿using System;
+using Server.Model.ChatModel;
+using Shared.DataClasses;
+using Shared.Utils;
+
+namespace Server.MessageSenders {
+    public static class UserSuccessfullyCreated {
+        private const int instruction = 200;
+
+        public static async void SendMessage(int index) {
+            BufferWriter bw = new BufferWriter();
+            bw.WriteInt(instruction);
+            bw.WriteInt(0);
+            await Network.instance.SendToClientAsync(index, bw.ToArray());
+        }
     }
 }
