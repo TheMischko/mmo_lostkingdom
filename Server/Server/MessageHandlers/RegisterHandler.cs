aacd76975a0dd963f00985591592d312ceee7eda
Miško Dvořák <michal.dvorak999@gmail.com>
2023-04-23 12:57:18 +0200

    Server: Added sending successful registration message.

--- Server/Server/MessageHandlers/RegisterHandler.cs
+++ Server/Server/MessageHandlers/RegisterHandler.cs
@@ -1,4 +1,6 @@
-﻿using Server.Model.UserModel;
+﻿using System;
+using Server.MessageSenders;
+using Server.Model.UserModel;
 using Server.Utils;
 using Shared.DataClasses;
 using Shared.Utils;
@@ -10,12 +12,15 @@ namespace Server.MessageHandlers {
             string login = reader.ReadString();
             string email = reader.ReadString();
             string passwordRaw = reader.ReadString();
-
+            Console.WriteLine("Creating a new account.");
             ResultInfo<long> result = await UserModel.instance.Register(login, email, passwordRaw);
             if (result.status == Status.Error) {
                 // Handle error.
+                Console.WriteLine("Error: " + result.message);
                 return;
             }
+            
+            UserSuccessfullyCreated.SendMessage(index);
         }
     }
 }
