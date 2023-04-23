29fe3091ef8c6d813cd6dd6d1487619b8cc814b4
Miško Dvořák <michal.dvorak999@gmail.com>
2023-04-23 12:53:42 +0200

    Client: Created handler for successful registration.

--- Assets/Scripts/Networking/MessageHandlers/SuccessCreatedAccountHandler.cs
+++ Assets/Scripts/Networking/MessageHandlers/SuccessCreatedAccountHandler.cs
@@ -1,5 +1,14 @@
-﻿namespace Networking.MessageHandlers {
-    public class SuccessCreatedAccountHandler {
+﻿using System;
+using Shared.DataClasses;
+using Shared.Utils;
+using UnityEngine;
+
+namespace Networking.MessageHandlers {
+    public static class SuccessCreatedAccountHandler {
+        public static event EventHandler Happened;
         
+        public static void Handle(BufferReader reader) {
+            Happened?.Invoke(null, EventArgs.Empty);
+        }
     }
 }
