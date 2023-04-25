2d35d8fbce37eaf5fbc555f5e0f99719f85a5c47
Miško Dvořák <michal.dvorak999@gmail.com>
2023-04-25 13:07:55 +0200

    Server: Added Error sender.

--- Server/Server/MessageSenders/ErrorSender.cs
+++ Server/Server/MessageSenders/ErrorSender.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System.Threading.Tasks;
+using Shared.Enums;
+using Shared.Utils;
+
+namespace Server.MessageSenders {
+    public static class ErrorSender {
+        private const int instruction = 400;
+
+        public static async Task SendMessage(int index, ErrorType type, string message) {
+            BufferWriter bw = new BufferWriter();
+            bw.WriteInt(instruction);
+            bw.WriteInt((int)type);
+            bw.WriteString(message);
+
+            await Network.instance.SendToClientAsync(index, bw.ToArray());
+        }
+    }
+}
