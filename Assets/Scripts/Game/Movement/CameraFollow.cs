3fa1b4387072e2e9f9888461653a1e4cfaecdb97
Miško Dvořák <michal.dvorak999@gmail.com>
2023-04-25 14:05:22 +0200

    Client: Script for camera following target.

---
+++ Assets/Scripts/Game/Movement/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Movement {
+    public class CameraFollow : MonoBehaviour {
+        public Transform target;
+        public float smoothing;
+        public bool isFollowing;
+
+        private void Update() {
+            if (isFollowing) {
+                Vector2 lerped = Vector2.Lerp((Vector2) transform.position, (Vector2) target.position, smoothing);
+                transform.position = new Vector3(lerped.x, lerped.y, transform.position.z);
+            }
+        }
+    }
+}
