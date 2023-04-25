5e306d4528543a02a5d48e9511813607699761d8
Miško Dvořák <michal.dvorak999@gmail.com>
2023-04-25 13:08:56 +0200

    Server: Send correct username on registration.

--- Server/Server/MessageSenders/LoginSuccessfulSender.cs
+++ Server/Server/MessageSenders/LoginSuccessfulSender.cs
@@ -5,11 +5,10 @@ using Shared.Utils;
 namespace Server.MessageSenders {
     public static class LoginSuccessfulSender {
         private const int instruction = 102;
-        public static async Task SendMessage(int index) {
+        public static async Task SendMessage(int index, string username) {
             BufferWriter bw = new BufferWriter();
             bw.WriteInt(instruction);
-            string userName = $"user-{index}";
-            UserData userData = new UserData(index, userName);
+            UserData userData = new UserData(index, username);
             bw.AddData(userData.ToBytes());
             
             await Network.instance.SendToClientAsync(index, bw.ToArray());
