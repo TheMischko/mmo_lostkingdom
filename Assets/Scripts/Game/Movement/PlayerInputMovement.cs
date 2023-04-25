fcf10474924585425aca605cb40fca97a320ab1c
Miško Dvořák <michal.dvorak999@gmail.com>
2023-04-25 14:05:01 +0200

    Client: Read player movement.

--- Assets/Scripts/Game/Movement/PlayerInputMovement.cs
+++ Assets/Scripts/Game/Movement/PlayerInputMovement.cs
@@ -1,9 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Game.Movement {
     public class PlayerInputMovement : BaseMovement {
+        [SerializeField] private InputActionReference movementHoriz;
+        [SerializeField] private InputActionReference movementVertical;
+
+        private Vector2 direction;
         protected void Awake() {
             base.Awake();
+            direction = Vector2.zero;
+            movementHoriz.action.performed += OnMovementHoriz;
+            movementVertical.action.performed += OnMovementVert;
+        }
+
+        private void OnMovementHoriz(InputAction.CallbackContext context) {
+            float dir = context.ReadValue<float>();
+            direction.x = dir;
+            Debug.Log(direction);
+            OnChangeDirection(direction);
+        }
+        private void OnMovementVert(InputAction.CallbackContext context) {
+            float dir = context.ReadValue<float>();
+            direction.y = dir;
+            Debug.Log(direction);
+            OnChangeDirection(direction);
         }
     }
 }
