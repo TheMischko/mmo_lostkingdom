b1e85681a690ad3a1743cf80954b8f86b2f5fb70
Miško Dvořák <michal.dvorak999@gmail.com>
2023-04-25 16:29:27 +0200

    Client: Added basic states to player.

--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 namespace Game {
     public class PlayerController:MonoBehaviour {
         public float movementSpeed = 1f;
+        public bool isPlayerControlled = false;
+        public PlayerState state = PlayerState.Idle;
         
         private Vector2 movementDir = Vector2.zero;
         private Rigidbody2D rigidbody;
@@ -17,6 +19,18 @@ namespace Game {
         }
 
         private void Update() {
+            switch (state) {
+                case PlayerState.Idle:
+                    if (!movementDir.Equals(Vector2.zero)) {
+                        state = PlayerState.Moving;
+                    }
+                    break;
+                case PlayerState.Moving:
+                    if (movementDir.Equals(Vector2.zero)) {
+                        state = PlayerState.Idle;
+                    }
+                    break;
+            }
             rigidbody.velocity = movementDir * movementSpeed;
         }
     }
